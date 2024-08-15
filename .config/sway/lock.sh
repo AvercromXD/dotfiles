816c28ab235b65325ff139814f783941fd3aa332
Ian <ian.schmetkamp@posteo.de>
2024-08-15 16:07:52 +0200

    Idle inhibitor off by default

---
+++ .config/sway/lock.sh
@@ -0,0 +1,15 @@
+
+#!/bin/sh
+
+# Times the screen off and puts it to background
+swayidle \
+    timeout 10 'swaymsg "output * dpms off"' \
+    resume 'swaymsg "output * dpms on"' &
+
+swaylock \
+	--clock \
+	--indicator \
+
+# Kills last background task so idle timer doesn't keep running
+kill %%
+
